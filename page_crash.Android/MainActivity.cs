13a322c31879aa0222b563c305368f61f01eae72
Drew Frisk <dfrisk@jjkeller.com>
2018-12-20 09:55:26 -0600

    initial commit for code to reproduce issue 4706 in xamarin.forms

---
+++ page_crash.Android/MainActivity.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Android.Util;
+
+namespace page_crash.Droid
+{
+    [Activity(Label = "page_crash", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        readonly string TAG = nameof(MainActivity);
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Log.Info(TAG, "Entered MainActivity.OnCreate()");
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            TrapException();
+
+            var formsApp = (this.Application as MainApplication).FormsApplication;
+
+            LoadApplication(formsApp);
+        }
+
+        protected override void OnPause()
+        {
+            Log.Info(TAG, "Entered MainActivity.OnPause()");
+
+            base.OnPause();
+        }
+
+        protected override void OnStop()
+        {
+            Log.Info(TAG, "Entered MainActivity.OnStop()");
+
+            base.OnStop();
+        }
+
+        protected override void OnDestroy()
+        {
+            Log.Info(TAG, "Entered MainActivity.OnDestroy()");
+
+            base.OnDestroy();
+        }
+
+        private void TrapException()
+        {
+            AndroidEnvironment.UnhandledExceptionRaiser += (sender, args) => OnUnhandledException(args.Exception);
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) => OnUnhandledException(args.ExceptionObject as Exception);
+            TaskScheduler.UnobservedTaskException += (sender, args) => OnUnhandledException(args.Exception);
+        }
+
+        private void OnUnhandledException(Exception ex)
+        {
+            Debugger.Break();
+        }
+    }
+}
---
+++ page_crash.Android/MainApplication.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+
+namespace page_crash.Droid
+{
+    [Application(Label = "page_crash_test")]
+    public class MainApplication : Android.App.Application
+    {
+        readonly string TAG = nameof(MainApplication);
+
+        public App FormsApplication { get; private set; }
+
+        protected MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+
+        }
+
+        public override void OnCreate()
+        {
+            Log.Info(TAG, "Entered MainApplication.OnCreate()");
+
+            base.OnCreate();
+
+            Forms.Init(this, null);//, savedInstanceState);
+            FormsApplication = new App();
+        }
+    }
+}
